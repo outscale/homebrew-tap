fe3db4713d77dd828c53148cc29e77b182b416cd
matthias.gatto <matthias.gatto@outscale.com>
2023-07-07 15:03:04 +0200

    initial commit
    Signed-off-by: matthias.gatto <matthias.gatto@outscale.com>

---
+++ Formula/oapi-cli.rb
@@ -0,0 +1,32 @@
+class OapiCli < Formula
+  desc "Official Outscale CLI providing connectors to Outscale API"
+  homepage "https://github.com/outscale/oapi-cli"
+  url "https://github.com/outscale/oapi-cli.git"
+  head "https://github.com/outscale/oapi-cli", branch: "master"
+  sha256 ""
+  license "BSD-3-Clause"
+  version "0.0.0"
+
+  depends_on "curl"
+  depends_on "json-c"
+  depends_on "cmake" => :build
+
+
+  def install
+    system "git", "submodule", "update", "--init"
+    system "make"
+    system "mkdir -p #{prefix}/bin"
+    system "cp ./oapi-cli #{prefix}/bin/"
+    system "mkdir -p #{prefix}/contrib/completions/bash/"
+    system "mkdir -p #{prefix}/contrib/completions/zsh/"
+    system "cp ./oapi-cli-completion.bash #{prefix}/contrib/completions/bash/"
+    system "echo autoload bashcompinit > #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
+    system "echo bashcompinit >> #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
+    system "echo source #{prefix}/contrib/completions/bash/oapi-cli-completion.bash >> #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
+  end
+
+  test do
+    str = shell_output("#{bin}/oapi-cli --help")
+    assert_match "Usage: oapi-cli [--help] CallName [options] [--Params ParamArgument]", str
+  end
+end
