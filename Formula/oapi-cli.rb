bcd99676d61e58a2fabf6d515f8bc7544c35446a
Ch.-David Blot <charles-david.blot@outscale.com>
2025-07-23 14:05:35 +0000

    🔖 release: release oapi-cli v0.10.0 (#9)

--- Formula/oapi-cli.rb
+++ Formula/oapi-cli.rb
@@ -1,30 +1,30 @@
 class OapiCli < Formula
   desc "Official Outscale CLI providing connectors to Outscale API"
   homepage "https://github.com/outscale/oapi-cli"
-  url "https://github.com/outscale/oapi-cli.git", tag: "v0.9.0"
-  head "https://github.com/outscale/oapi-cli", branch: "v0.9.0"
+  url "https://github.com/outscale/oapi-cli.git", tag: "v0.10.0"
 
-  sha256 ""
   license "BSD-3-Clause"
-  version "0.9.0"
 
+  depends_on "pkgconf" => :build
   depends_on "curl"
   depends_on "json-c"
-  depends_on "cmake" => :build # might not be useful annymore
-  depends_on "pkgconf" => :build
 
+  conflicts_with "oapi-cli-git", because: "each are different versions of the same formula"
 
   def install
     system "git", "submodule", "update", "--init"
     system "make"
-    system "mkdir -p #{prefix}/bin"
-    system "cp ./oapi-cli #{prefix}/bin/"
-    system "mkdir -p #{prefix}/contrib/completions/bash/"
-    system "mkdir -p #{prefix}/contrib/completions/zsh/"
-    system "cp ./oapi-cli-completion.bash #{prefix}/contrib/completions/bash/"
-    system "echo autoload bashcompinit > #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
-    system "echo bashcompinit >> #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
-    system "echo source #{prefix}/contrib/completions/bash/oapi-cli-completion.bash >> #{prefix}/contrib/completions/zsh/oapi-cli-completion.zsh"
+    bin.install "oapi-cli"
+    mkdir_p "#{prefix}/contrib/completions/bash/"
+    mkdir_p "#{prefix}/contrib/completions/zsh/"
+    cp "./oapi-cli-completion.bash", "#{prefix}/contrib/completions/bash/"
+
+    zshcomp = (prefix/"contrib/completions/zsh/oapi-cli-completion.zsh")
+    zshcomp.write <<~ZSH
+      autoload bashcompinit
+      bashcompinit
+      source #{prefix}/contrib/completions/bash/oapi-cli-completion.bash
+    ZSH
   end
 
   test do
