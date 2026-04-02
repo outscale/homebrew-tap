258141717a93d3a44b15c7b4f48e43de064b0a2e
Ch.-David Blot <charles-david.blot@outscale.com>
2026-04-02 15:39:49 +0200

    ✨ feat: add octl

---
+++ Formula/octl.rb
@@ -0,0 +1,25 @@
+class Octl < Formula
+  desc "Modern CLI for Outscale"
+  homepage "https://github.com/outscale/octl"
+  url "https://github.com/outscale/octl.git",
+    tag:      "v0.0.20",
+    revision: "c4ceab88e6d4a5f6d778c669cbf63c704a91c9f6"
+  license "BSD-3-Clause"
+  head "https://github.com/outscale/octl.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "mod", "edit", "-go=1.24"
+    system "go", "mod", "tidy"
+    ldflags = ["-s", "-w", "-X=github.com/outscale/octl/pkg/version.Version=v#{version}"]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"octl", shell_parameter_format: :cobra)
+  end
+
+  test do
+    run_output = shell_output("#{bin}/octl 2>&1")
+    assert_match "One CLI to rule them all", run_output
+  end
+end
