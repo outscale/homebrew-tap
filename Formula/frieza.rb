d9fb84b5f909b6b41b2af919205ea39864da4893
Ch.-David Blot <charles-david.blot@outscale.com>
2026-05-11 13:46:46 +0200

    ⬆️ feat(frieza): Update formula version

--- Formula/frieza.rb
+++ Formula/frieza.rb
@@ -1,17 +1,19 @@
 class Frieza < Formula
-  desc "A tool to cleanup your cloud resources"
+  desc "Tool to cleanup your cloud resources"
   homepage "https://github.com/outscale/frieza"
-  url "https://github.com/outscale-dev/frieza.git", :tag => "v0.6.0"
+  url "https://github.com/outscale/frieza.git", tag: "v0.14.1"
   license "BSD-3-Clause"
+  head "https://github.com/outscale/frieza.git", branch: "main"
 
   depends_on "go" => :build
-  
+
   def install
     system "make", "build"
     bin.install "cmd/frieza/frieza"
   end
-  
+
   test do
-    assert_match "frieza", shell_output("#{bin}/frieza --help")
+    run_output = shell_output("#{bin}/frieza 2>&1")
+    assert_match "Cleanup your cloud ressources.", run_output
   end
 end
